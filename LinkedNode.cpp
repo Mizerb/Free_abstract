e2e346ffce12a9faa7b8e36c50a0aa8ee3ed5055
Par_Group <bluekkid@gmail.com>
2016-04-27 17:23:00 +0000

    New thing

---
+++ LinkedNode.cpp
@@ -0,0 +1,38 @@
+
+
+template<class T>
+class LinkedNode
+{
+    public:
+        T data;
+        Node()
+        {
+            //default contructor
+        }
+        
+        
+        Node(T indata)
+        {
+            data = indata
+            next = NULL;
+            
+        }
+        
+        Node<T>* Node<T>::DeleteAfter()
+        {
+              // store the next Node in a temporary Node
+            Node<T>* tempNode = next;
+            // check if there is a next node
+            if(next != NULL)
+                next = next->next;
+
+            return tempNode;
+        }
+        
+    private:
+        Node<T> * next;
+        
+    
+    
+    
+}
